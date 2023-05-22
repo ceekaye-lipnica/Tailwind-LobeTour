b2e40618e03fa53324566647eee58d98b1dc4924
tsutrof <jaysonfortus0815@gmail.com>
2023-05-22 15:50:44 +0800

    updated

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__actions");
+const header = document.querySelector(".header");
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("open");
+	mobileNav.classList.toggle("open");
+    header.classList.toggle("open");
+});
+
+const links = document.querySelectorAll(".footer-items h4");
+links.forEach((baba) => {
+	baba.addEventListener("click", () => {
+		baba.nextElementSibling.classList.toggle("open");
+		baba.querySelector("i").classList.toggle("open");
+	});
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#323232',
+        light: '#ffffff',
+        primary: '#04ddb2',
+        grey: '#777777',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+}
+
